6b5e9e72a5dcb638792a3abb43d8761d8c2359c6
Lord Niels N. Horn <i.am@nilq.dk>
2016-10-28 22:13:45 +0200

    Add working routes

--- src/prism.cr
+++ src/prism.cr
@@ -5,19 +5,20 @@ require "kemal"
 module Prism
   # load index
   get "/" do
+    puts "index.ecr : render"
     load_index
   end
 
   # load stylesheet
   get "/style/:path" do |env|
     env.response.content_type = "text/css"
-    File.read load_style(env.params.query["path"])
+    File.read load_style(env.params.url["path"])
   end
 
   # load scripts
   get "/script/:path" do |env|
     env.response.content_type = "application/javascript"
-    File.read load_style(env.params.query["path"])
+    File.read load_script(env.params.url["path"])
   end
 
   Kemal.run
