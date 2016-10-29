3626cdda2f5668bd57159a3322a0ec998cca165f
Lord Niels N. Horn <i.am@nilq.dk>
2016-10-29 14:48:19 +0200

    Add simple game

--- static/js/game.js
+++ static/js/game.js
@@ -1,15 +1,45 @@
 var SimpleGame = (function () {
     function SimpleGame() {
-        this.game = new Phaser.Game(800, 800, Phaser.AUTO, 'content', {
-            preload: this.preload, create: this.create
+        this.jump_timer = 0;
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
+            preload: this.preload,
+            create: this.create,
+            update: this.update,
+            render: this.render
         });
     }
     SimpleGame.prototype.preload = function () {
-        this.game.load.image('logo', '../../assets/profile.png');
+        this.game.load.image("dude", "../../assets/dude.png");
+        this.game.load.image("background", "../../assets/background.png");
     };
     SimpleGame.prototype.create = function () {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.bg = this.game.add.tileSprite(0, 0, 800, 600, "background");
+        this.game.physics.arcade.gravity.y = 300;
+        this.player = this.game.add.sprite(32, 320, "dude");
+        this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+        this.player.body.collideWorldBounds = true;
+        this.player.body.gravity.y = 1000;
+        this.player.body.maxVelocity.y = 500;
+        this.player.body.setSize(20, 32, 5, 16);
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+        this.jump_button = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    };
+    SimpleGame.prototype.update = function () {
+        this.player.body.velocity.x = 0;
+        if (this.cursors.left.isDown) {
+            this.player.body.velocity.x = -150;
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.body.velocity.x = 150;
+        }
+        if (this.jump_button.isDown && this.player.body.onFloor()) {
+            this.player.body.velocity.y = -500;
+            this.jump_timer = this.game.time.now + 750;
+        }
+    };
+    SimpleGame.prototype.render = function () {
+        this.game.debug.bodyInfo(this.player, 16, 24);
     };
     return SimpleGame;
 }());
--- static/js/src/game.ts
+++ static/js/src/game.ts
@@ -1,29 +1,65 @@
 class SimpleGame {
 
     constructor() {
-        this.game = new Phaser.Game(800, 800, Phaser.AUTO, 'content', {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
           preload: this.preload,
           create:  this.create,
           update:  this.update,
-
+          render:  this.render,
         });
     }
 
     game: Phaser.Game;
+    player: Phaser.Sprite;
+
+    bg: Phaser.TileSprite;
+    cursors: Phaser.CursorKeys;
+
+    jump_timer = 0;
+    jump_button: Phaser.Key;
 
     preload() {
-        this.game.load.image('logo', '../../assets/profile.png');
+        this.game.load.image("dude", "../../assets/dude.png");
+        this.game.load.image("background", "../../assets/background.png");
     }
 
     create() {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        this.bg = this.game.add.tileSprite(0, 0, 800, 600, "background");
+
+        this.game.physics.arcade.gravity.y = 300;
+
+        this.player = this.game.add.sprite(32, 320, "dude");
+        this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+
+        this.player.body.collideWorldBounds = true;
+        this.player.body.gravity.y = 1000;
+        this.player.body.maxVelocity.y = 500;
+        this.player.body.setSize(20, 32, 5, 16);
+
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+        this.jump_button = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     }
 
     update() {
+      this.player.body.velocity.x = 0;
 
+      if(this.cursors.left.isDown) {
+        this.player.body.velocity.x = -150;
+      } else if(this.cursors.right.isDown) {
+        this.player.body.velocity.x = 150;
+      }
+
+      if(this.jump_button.isDown && this.player.body.onFloor()) {
+        this.player.body.velocity.y = -500;
+        this.jump_timer = this.game.time.now + 750;
+      }
     }
 
+    render() {
+      this.game.debug.bodyInfo(this.player, 16, 24);
+    }
 }
 
 window.onload = () => {
