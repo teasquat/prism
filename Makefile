dbe2447ad662cf893095679b62ded3b046711bf1
Lord Niels N. Horn <i.am@nilq.dk>
2016-10-29 12:32:54 +0200

    Add room-based socket

--- Makefile
+++ Makefile
@@ -2,8 +2,15 @@ build:
 	crystal build --release src/prism.cr
 	tsc
 
-serve:
+
+run:
 	./prism
+
+serve:
+	./server
+
+socket:
+	crystal build --release tools/server.cr
 
 setup:
 	sudo npm install typings
---
+++ tools/server.cr
@@ -0,0 +1,21 @@
+require "kemal"
+
+ROOMS = {} of String => Array(HTTP::WebSocket)
+
+ws "/rooms" do |socket, env|
+  room_id = env.params.query["id"]
+
+  ROOMS[room_id] << socket
+
+  socket.on_message do |m|
+    ROOMS[room_id].each { |sock|
+      sock.send m
+    }
+  end
+
+  socket.on_close do
+    ROOMS[room_id].delete socket
+  end
+end
+
+Kemal.run
